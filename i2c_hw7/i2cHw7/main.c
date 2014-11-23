6e6fe85ac2779058dce82358432e46b2302fb1a2
DestinysChild <guonaive@gmail.com>
2014-11-23 09:29:11 +0800

    i2chw8(2)

--- i2c_hw7/i2cHw7/main.c
+++ i2c_hw7/i2cHw7/main.c
@@ -5,6 +5,233 @@
 /*
 int main()
 {
+    int m,n;
+    scanf("%d %d",&m,&n);
+    //int b[9][9];
+    int live[9][9];
+    int tmp[9][9];
+    int i,j;
+    for(i=0;i<9;i++){
+        for(j=0;j<9;j++){
+            live[i][j]=0;
+            tmp[i][j]=0;
+            //b[i][j]=0;
+        }
+    }
+    live[4][4]=m;
+    //b[4][4]=m;
+
+    while(n--){
+
+        for(i=0;i<9;i++){
+            for(j=0;j<9;j++){
+                int bnow=live[i][j];
+                if(bnow>0){
+                    tmp[i][j] +=2*bnow;
+                    tmp[i-1][j-1] +=bnow;
+                    tmp[i-1][j] +=bnow;
+                    tmp[i-1][j+1] +=bnow;
+
+                    tmp[i][j-1] +=bnow;
+                    tmp[i][j+1] +=bnow;
+
+                    tmp[i+1][j-1] +=bnow;
+                    tmp[i+1][j] +=bnow;
+                    tmp[i+1][j+1] +=bnow;//tmp[i][j] +=bnow;tmp[i][j] +=bnow;
+
+                }
+                //tmp[i][j]=0;
+            }
+        }
+        for(i=0;i<9;i++){
+            for(j=0;j<9;j++){
+                //b[i][j] += tmp[i][j]+live[i][j];
+                live[i][j]=tmp[i][j];
+                tmp[i][j]=0;
+            }
+        }
+    }
+    for(i=0;i<9;i++){
+       for(j=0;j<9;j++){
+            printf("%d",live[i][j]);
+            if(j<8) printf(" ");
+            else printf("\n");
+            }
+    }
+    return 0;
+}
+
+int main()
+{
+    int a[100][100];
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int i,j;
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    int sum=0;
+    while(sum<r+c-1){
+        //for(i=0;i<=sum;i++){
+            for(j=0;j<=sum;j++){
+                    if(j<r && sum-j<c)
+                        printf("%d\n",a[j][sum-j]);
+            }
+        //}
+        sum++;
+    }
+    return 0;
+}
+
+//2->16
+int main()
+{
+    int n;
+    scanf("%d\n",&n);
+    int cnt=0;
+
+    while(cnt<n){
+        cnt++;
+
+        char re[10005];
+        gets(re);
+        //gets(re);
+        int len = strlen(re);
+        //int i=len-1
+//if(cnt==12){
+        printf("\n%s   %d    ",re,len);
+//}
+        if(len%4==1)
+            printf("1");
+        else if(len%4==2){
+            switch(re[1]){
+                case '0':printf("2");break;
+                case '1':printf("3");break;
+            }
+        }
+        else if(len%4==3){
+            switch(re[1]){
+                case '0':switch(re[2]){
+                            case '0':printf("4");break;
+                            case '1':printf("5");break;
+                        }
+                        break;
+                case '1':switch(re[2]){
+                            case '0':printf("6");break;
+                            case '1':printf("7");break;
+                        }
+                        break;
+            }
+        }
+        if(len<4)
+            return 0;
+
+        int bit4=len%4;
+        while(bit4<len){
+            int value = (re[bit4]-'0')*8 +(re[bit4+1]-'0')*4 + (re[bit4+2]-'0')*2 + (re[bit4+3]-'0');
+            if(value<10)
+                printf("%d",value);
+            else{
+                switch(value){
+                            case 10:printf("A");break;
+                            case 11:printf("B");break;
+                            case 12:printf("C");break;
+                            case 13:printf("D");break;
+                            case 14:printf("E");break;
+                            case 15:printf("F");break;
+                            }
+            }
+            bit4 +=4;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int main()
+{
+    char re[101];
+    gets(re);
+    //gets(re);
+    int len = strlen(re);
+    int i=0;
+    while(i<len/2){
+        if(re[i]!=re[len-1-i]){
+            printf("no");
+            return 0;
+        }
+        i++;
+    }
+    printf("yes");
+    return 0;
+}
+
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int cnt=0;
+    int pn=4;
+    while(pn<n){
+        int sum=1;
+        int i;
+        for(i=2;i<=sqrt(pn);i++){
+            if(pn%i==0){
+                sum+=i;
+                if(pn/i>i){
+                    sum+=pn/i;
+                }
+            }
+        }
+        if(sum==pn){
+            printf("%d ",pn);
+            cnt++;
+        }
+        pn+=2;
+    }
+    if(cnt==0){
+        printf("None");
+        return 0;
+    }
+    //printf("%d",cnt);
+    return 0;
+}
+
+
+int main()
+{
+    double x,a ,e;
+    scanf("%lf %lf %lf",&x,&a,&e);
+    //printf("\n%lf %lf %lf\n",x,a,e);
+    if(x<0)
+        x=-x;
+    if(x<e){
+        printf("%d",0);
+        return 0;
+    }
+
+    int flag=0;
+    int n=0;
+    double value=1;
+    while(!flag){
+        double tmp = x/value;
+        //printf("\n%d  %lf\n",n,tmp);
+        if(tmp>=e){;}
+        else{
+            flag=1;
+            break;
+        }
+        value *=a;
+        n++;
+    }
+    printf("%d",--n);
+    return 0;
+}
+
+int main()
+{
     double e;
     scanf("%lf",&e);
     double low=-10;
